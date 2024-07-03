2deb32fa7b96dafa8e97c749b2d8848af41a7de1
Brendan Armstrong <brendanarmstrong@proton.me>
2024-07-02 21:09:03 -0400

    feat: added bang for reverse sort

--- lua/sortvis/config.lua
+++ lua/sortvis/config.lua
@@ -1,8 +1,11 @@
 M = {}
 function M.setup()
   vim.api.nvim_create_user_command("SortVis", function(args)
-    local largs = args.fargs[1]
-    require("sortvis").sort_visual_block(largs)
-  end, {range = true, nargs = "*"})
+    if args.bang then
+      require("sortvis").sort_visual_block("rev")
+    else
+      require("sortvis").sort_visual_block()
+    end
+  end, {range = true, bang = true})
 end
 return M
